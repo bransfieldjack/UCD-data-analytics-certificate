436d94fcafbdf5f12b4ce5ce7b96e5a4929f127c
JACK BRANSFIELD <bransfieldjack@gmail.com>
2021-04-07 07:07:32 +0100

    Updating readme, adding more testing code.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # UCD-data-analytics-certificate
+
+### Technology used:
+- conda (local package/environment management)
+- VS Code, local text editor. (Pycharm for project delivery but my license expired)
+- Google collab for quick prototyping
+- Jupyter notebooks
+#### Packages:
+- Python requests library for API data retrieval 
+- Pandas for data manipulation
+- Matplotlib for visualisation
+
+### Git setup
--- testing.py
+++ testing.py
@@ -1,26 +1,31 @@
 import requests, json
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def main():
 
+    # specify the required format of the data requested from the api
     headers = {
         "Content-type": "application/json",
         "Content-Type": "application/json;charset=UTF-8",
     }
 
-    data = {
-        "method": "GET",
-        "responseType": "blob",
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json;charset=UTF-8",
-        },
-        "data": {"dataset_id": "7241"},
-    }
+    # make the request
+    request = requests.get("https://global-warming.org/api/co2-api", headers=headers)
 
-    r = requests.get("https://api.github.com/users/octocat/orgs", headers=headers)
-    print(r)
+    # assign the json result to a variable called 'data'
+    data = request.json()
+
+    # confirm the data has the expected type
+    print(type(data))
+
+    # coerce the data into a dataframe
+    df = pd.DataFrame.from_dict(data, orient="columns")
+
+    print(df)
 
 
+# python specific, allows explicit call
 if __name__ == "__main__":
     main()
